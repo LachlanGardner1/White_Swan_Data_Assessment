8b9e8f6002b7f0b24c549f503d7ec0447e3ab9a1
Lachlan Gardner <lachlan.gardner2@hotmail.com>
2024-11-01 12:03:17 +0000

    bot automation script, drivers, exmaple data files, logging config, requirements.txt

---
+++ main/bot_automation.py
@@ -0,0 +1,130 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+import pandas as pd
+from random import randint
+import logging
+import logging.config
+import yaml
+import traceback
+from time import sleep
+
+
+#Update chromdriver path to relevant operating system if necessary
+driver_path = 'driver/chromedriver-win64.exe'
+url = 'https://www.swiftbet.com.au/racing'
+service = Service(executable_path=driver_path)
+options = webdriver.ChromeOptions()
+options.add_argument("log-level=3")
+driver = webdriver.Chrome(options=options, service=service)
+
+def get_logger(config):
+    logging.config.dictConfig(config)
+    logger = logging.getLogger("development")
+    return logger
+
+def check_price(price):
+    if price == '':
+        return 'SCRATCHED'
+    elif price == 'SP':
+        return 'N/A'
+    return price
+
+def main(logger):
+    try:
+        logger.info("Begin Bot Automation")
+        #read races data csv
+        races_df = pd.read_csv('data/races_data.csv')
+        
+        #select random row
+        random_row = randint(0, len(races_df.index))
+        selected_row = races_df.iloc[random_row]
+
+        #get track name and number
+        track_name = selected_row['Track Name'].split(" ")
+        track_name = track_name[:-1]
+        # track_name = "-".join(track_name).lower()
+        # race_number = selected_row['Race Number']
+        track_name = 'the-gardens'
+        race_number = '12'
+        # race_type = selected_row['Race URL'].split("racing/")[1].split("/")[0]
+        race_type = 'greyhounds'
+        logging.info(f"Clicking on randomly selected race track: {track_name}, race number: {race_number}, race type: {race_type}")
+
+        all_racers = []
+        try:
+            #launch url and wait for loading
+            driver.get(url)
+            driver.maximize_window()
+            WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH,  f"//*[contains(@href, '{track_name}') and contains(@href, 'race-{race_number}') and contains(@href, '{race_type}')]")))
+            
+            #click randomly selected race and wait for page to load
+            driver.find_element(By.XPATH,  f"//*[contains(@href, '{track_name}') and contains(@href, 'race-{race_number}') and contains(@href, '{race_type}')]").click()
+            sleep(10)
+        except TimeoutException as e:
+            logger.error(e)
+            logger.error("Page took too long to load")
+            raise e
+
+        #find amount of racers
+        all_racers = driver.find_elements(By.XPATH, "//div[contains(@data-fs-title, 'racer_name')]")
+        racers_count = len(all_racers)
+        performed_bets = []
+        index = 0
+        for i in range(racers_count):
+            current_racer = {}
+            
+            #find all racers elements to refresh the DOM
+            all_racers = driver.find_elements(By.XPATH, "//div[contains(@data-fs-title, 'racer_name')]")
+
+            #Get fixed win and fixed place prices for all racers
+            fixed_win_prices = driver.find_elements(By.XPATH, "//div[contains(@data-fs-title, '0-price_button')]")
+            fixed_place_prices = driver.find_elements(By.XPATH, "//div[contains(@data-fs-title, '1-price_button')]")
+
+            #select fixed win price for current racer
+            fixed_win_price = fixed_win_prices[index].find_element(By.TAG_NAME, "span").text
+            fixed_win_price = check_price(fixed_win_price)
+                
+            #select fixed place price for current racer
+            fixed_place_price = fixed_place_prices[index].find_element(By.TAG_NAME, "span").text
+            fixed_place_price = check_price(fixed_place_price)
+
+            #store current racer information in performed_bets
+            current_racer['Racer'] = all_racers[index].text
+            current_racer['Fixed Win Price'] = fixed_win_price
+            current_racer['Fixed Place Price'] = fixed_place_price
+            
+            logger.debug(f"appending betting information {all_racers[index].text}")
+            performed_bets.append(current_racer)
+            index += 1
+
+        logger.debug(performed_bets)
+
+        if performed_bets != []:
+            #convert performed_bets into dataframe and export to CSV with track_name and race_number
+            df_performed_bets = pd.DataFrame(performed_bets)
+            df_performed_bets.to_csv(f'data/{track_name}-race-{race_number}.csv', index=False) 
+        
+            logger.info(f"Performed bets retrieved and stored into {track_name}-race-{race_number}.csv")
+
+        driver.quit()
+    except Exception as e:
+        driver.quit()
+        logger.error(e)
+        logger.error("error during processing")
+        raise e
+
+if __name__ == '__main__':
+    #get logger config and load logger
+    with open('config/logger_config.yaml','rt') as f:
+            config=yaml.safe_load(f.read())
+    logger = get_logger(config)
+    try:
+        main(logger)
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        logger.error(e)
+        logger.error("an error has occured")
--- main/data_scraping.py
+++ main/data_scraping.py
@@ -1,52 +1,105 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 import pandas as pd
-import regex as re
-from time import sleep
+import logging
+import logging.config
+import yaml
+import traceback
 
-url = 'https://www.swiftbet.com.au/racing/all/2024-10-30'
-path = '../driver/chromedriver.exe'
-service = Service(executable_path=path)
-driver = webdriver.Chrome(service=service)
 
-def main():
+#Update chromdriver path to relevant operating system if necessary
+driver_path = 'driver/chromedriver-win64.exe'
+url = 'https://www.swiftbet.com.au/racing'
+service = Service(executable_path=driver_path)
+options = webdriver.ChromeOptions()
+options.add_argument("log-level=3")
+driver = webdriver.Chrome(options=options, service=service)
+
+def get_logger(config):
+    logging.config.dictConfig(config)
+    logger = logging.getLogger("development")
+    return logger
+
+def define_race(current_race, race_number, url, race_time):
+    #Common method to add race_number, url and race_time to current_race
+    current_race["Race Number"] = race_number
+    current_race["Race URL"] = url
+    current_race["Race Time"] = race_time
+    return current_race
+
+def main(logger):
     try:
-        number_of_titles = len(driver.find_elements(By.CLASS_NAME, "e17e4d890.exx10s30.css-lz7t06-Text-Text-Link-Link-MeetingItem-MeetingItem__MeetingName-MeetingItem.ea6hjv30"))
-        all_races = driver.find_elements(By.CLASS_NAME, "e15267q10.css-dr0t2h-TableRow-TableRow-TableRow-RacesRow-RacesRow-RacesRow.e17s2h8j0")
-        races_dictionary = {}
+        logger.info("Begin Data Scraping")
+
+        #Open URL and wait for presence of track names
+        try:
+            driver.get(url)
+            WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "e17e4d890.exx10s30.css-lz7t06-Text-Text-Link-Link-MeetingItem-MeetingItem__MeetingName-MeetingItem.ea6hjv30")))
+        except TimeoutException as e:
+            logger.error(e)
+            logger.error("Page took too long to load")
+            raise e
+        
+        #find number of tracks
+        number_of_tracks = len(driver.find_elements(By.CLASS_NAME, "e17e4d890.exx10s30.css-lz7t06-Text-Text-Link-Link-MeetingItem-MeetingItem__MeetingName-MeetingItem.ea6hjv30"))
+        
+        races_list = []
         index = 0
-        for i in  range(number_of_titles):
-            all_titles = driver.find_elements(By.CLASS_NAME, "e17e4d890.exx10s30.css-lz7t06-Text-Text-Link-Link-MeetingItem-MeetingItem__MeetingName-MeetingItem.ea6hjv30")
-            title = all_titles[index]
-            race_title = title.text
+        for i in range(number_of_tracks):
+
+            #Find all tracks and races to refresh the DOM
+            all_tracks = driver.find_elements(By.CLASS_NAME, "e17e4d890.exx10s30.css-lz7t06-Text-Text-Link-Link-MeetingItem-MeetingItem__MeetingName-MeetingItem.ea6hjv30")
+            all_races = driver.find_elements(By.CLASS_NAME, "e15267q10.css-dr0t2h-TableRow-TableRow-TableRow-RacesRow-RacesRow-RacesRow.e17s2h8j0")
             
-            current_race = {}
-            current_race[race_title] = {}
+            #Select current race track
+            track = all_tracks[index]
+            race_track = track.text
             
+            #Select all races for current track
             races = all_races[index].find_elements(By.TAG_NAME, 'a')
             race_number = 1
             for race in races:
+                logger.debug(f"Beginning scrape for race_track: {race_track}")
+                current_race = {}
+
+                #Add Track Name, Race Number, Race URL, and Race Time for current track
+                current_race["Track Name"] = race_track
+                race_url = race.get_attribute('href')
                 if len(race.find_elements(By.XPATH, ".//div[@type='default']")) > 0:
-                    current_race[race_title][f"Race {race_number}"] = {"race_url": race.get_attribute('href'), "race_time": race.text}
+                    races_list.append(define_race(current_race, race_number, race_url, race.text))
                 else:
-                    current_race[race_title][f"Race {race_number}"] = {"race_url": race.get_attribute('href'), "race_time": "Race Completed"}
+                    races_list.append(define_race(current_race, race_number, race_url,"Race Completed"))
                 race_number += 1
-            races_dictionary.update(current_race)
+            
             index += 1
-        races_df = pd.DataFrame.from_dict(races_dictionary)
-        print(races_df.describe())
-        print(races_df.columns)
-        races_df.to_csv('../data/out.csv', index=False) 
-    except Exception as e:
-        print(e)
-        print("error during processing")
-if __name__ == '__main__':
-    try:
-        driver.get(url)
-        sleep(15)
-        main()
+
+        logger.debug(races_list)
+
+        if races_list != []:
+            #Transform races_list to dataframe and export to CSV as race_data.csv
+            df_races_data = pd.DataFrame(races_list)
+            df_races_data.to_csv('data/races_data.csv', index=False)
+
+            logger.info("Races data retrieved and stored into races_data.csv")
         driver.quit()
     except Exception as e:
-        print(e)
-        print("an error has occured")
+        driver.quit()
+        logger.error(e)
+        logger.error("error during processing")
+        raise e
+
+if __name__ == '__main__':
+    #get logger config and load logger
+    with open('config/logger_config.yaml','rt') as f:
+            config=yaml.safe_load(f.read())
+    logger = get_logger(config)
+    try:
+        main(logger)        
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        logger.error(e)
+        logger.error("an error has occured")
